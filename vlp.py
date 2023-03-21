51ead6265875a4c7b7246a7a095eef94b103ca44
JASHVANTH-SR <128201154+JASHVANTH-SR@users.noreply.github.com>
2023-03-21 23:33:40 +0530

    Update vlp.py

--- vlp.py
+++ vlp.py
@@ -116,13 +116,14 @@ st.image(image)
 st.title('''
 ## The Water Quality Analysis App''')
 runvoice("The Water Quality Analysis App")
-
+time.sleep(10)
 st.markdown('''### This is the **Study App** created in Streamlit using the **pandas-profiling** library.
 ****Credit:**** App built in `Python` + `Streamlit` by [JASHVANTH S R ](https://www.linkedin.com/in/jashvanth-s-r-476646213)[HARUL GANESH S B ](https://www.linkedin.com/in/harul-ganesh/)[BALAJI S ](https://www.linkedin.com/in/balaji-s-csbs-dept-03790a202/)[GOWTHAM H](https://www.linkedin.com/in/gowtham-haribabu-9425861bb/)
 ---
 ''')
 runvoice("This is the Software used to study the Characteristics of Water. This software is created in Streamlit using Scikit-learn and plotly package.\
     Courtesy :  Software built in 'Python' and 'Streamlit' by JASHVANTH S R,BALAJI S,HARUL GANESH S B,GOWTHAM H")
+time.sleep(10)
 try:
     uploaded_file=st.text_input("Enter the dataset excel")
     df=pd.read_excel(uploaded_file)
@@ -165,6 +166,7 @@ if uploaded_file is not None:
        total suspended solids, and turbidity. Chemical parameters tend to pose more of a chronic health risk through\
         buildup of heavy metals although some components like nitrates/nitrites and arsenic can have a more immediate\
          impact. Physical parameters affect the aesthetics and taste of the drinking water and may complicate the removal of microbial pathogens.")
+    time.sleep(10)
     d= pd.DataFrame(df['potability'].value_counts())
     fig = px.pie(d,values='potability',names=['Not Potable','Potable'],hole=0.4,opacity=0.6,
                 color_discrete_sequence=[colors_green[3],colors_blue[3]],
@@ -197,6 +199,7 @@ if uploaded_file is not None:
     runvoice("Total dissolved solids (TDS) is a measure of the dissolved combined content of all inorganic and organic substances present in a liquid in molecular,\
      ionized, or micro-granular (colloidal sol) suspended form.\
      TDS concentrations are often reported in parts per million (ppm). Water TDS concentrations can be determined using a digital meter.")
+    time.sleep(10)
     fig = px.histogram(df,x='TDS',y=df['TDS'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -232,6 +235,7 @@ if uploaded_file is not None:
         some forms, nitrates and nitrites can be hazardous. However, they may also have health benefits.")
     runvoice("Nitrates and nitrites are compounds that occur naturally in the human body and some foods, such as vegetables. Manufacturers also add them to processed foods, such as bacon, to preserve them and make them last longer.\
     In some forms, nitrates and nitrites can be hazardous. However, they may also have health benefits.")
+    time.sleep(10)
     fig = px.histogram(df,x='NO2+NO3',y=df['NO2+NO3'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -260,6 +264,7 @@ if uploaded_file is not None:
      Calcium is a mineral your body needs to build and maintain strong bones and to carry out many important functions. Calcium is the most abundant mineral in the body. Almost all calcium in the body is stored in bones and teeth, giving them structure and hardness.")
     runvoice("Calcium is a chemical element with the symbol Ca and atomic number 20. As an alkaline earth metal, calcium is a reactive metal that forms a dark oxide-nitride layer when exposed to air.\
      Calcium is a mineral your body needs to build and maintain strong bones and to carry out many important functions. Calcium is the most abundant mineral in the body. Almost all calcium in the body is stored in bones and teeth, giving them structure and hardness.")
+    time.sleep(10)
     fig = px.histogram(df,x='Ca',y=df['Ca'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -277,13 +282,14 @@ if uploaded_file is not None:
 
 
     # In[10]:
-
+    
     st.markdown("### Magnesium")
     st.image(Image.open("Magnesium.png"))
     st.markdown("Magnesium is a chemical element with the symbol Mg and atomic number 12. It is a shiny gray metal having a low density, low melting point and high chemical reactivity.\
         Magnesium is a cofactor in more than 300 enzyme systems that regulate diverse biochemical reactions in the body, including protein synthesis, muscle and nerve function, blood glucose control, and blood pressure regulation [1-3]. Magnesium is required for energy production, oxidative phosphorylation, and glycolysis.")
     runvoice("Magnesium is a chemical element with the symbol Mg and atomic number 12. It is a shiny gray metal having a low density, low melting point and high chemical reactivity.\
         Magnesium is a cofactor in more than 300 enzyme systems that regulate diverse biochemical reactions in the body, including protein synthesis, muscle and nerve function, blood glucose control, and blood pressure regulation [1-3]. Magnesium is required for energy production, oxidative phosphorylation, and glycolysis.")
+    time.sleep(10)
     fig = px.histogram(df,x='Mg',y=df['Mg'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -312,6 +318,7 @@ if uploaded_file is not None:
         It helps with the function of nerves and muscles. It also helps to keep the right balance of fluids in your body. Your kidneys control how much sodium is in your body. If you have too much and your kidneys can't get rid it, sodium builds up in your blood. This can lead to high blood pressure.")
     runvoice("Sodium is a chemical element with the symbol Na and atomic number 11. It is a soft, silvery-white, highly reactive metal. Sodium is an alkali metal, being in group 1 of the periodic table. Its only stable isotope is ²³Na. \
         It helps with the function of nerves and muscles. It also helps to keep the right balance of fluids in your body. Your kidneys control how much sodium is in your body. If you have too much and your kidneys can't get rid it, sodium builds up in your blood. This can lead to high blood pressure.")
+    time.sleep(10)
     fig = px.histogram(df,x='Na',y=df['Na'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -336,6 +343,7 @@ if uploaded_file is not None:
         Potassium is an essential mineral that is needed by all tissues in the body. It is sometimes referred to as an electrolyte because it carries a small electrical charge that activates various cell and nerve functions. Potassium is found naturally in many foods and as a supplement.")
     runvoice("Potassium is the chemical element with the symbol K and atomic number 19. It is a silvery white metal that is soft enough to easily cut with a knife.\
         Potassium is an essential mineral that is needed by all tissues in the body. It is sometimes referred to as an electrolyte because it carries a small electrical charge that activates various cell and nerve functions. Potassium is found naturally in many foods and as a supplement.")
+    time.sleep(10)
     fig = px.histogram(df,x='K',y=df['K'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -359,6 +367,7 @@ if uploaded_file is not None:
         Chlorine has a pungent, irritating odor similar to bleach that is detectable at low concentrations. The density of chlorine gas is approximately 2.5 times greater than air, which will cause it to initially remain near the ground in areas with little air movement.")
     runvoice("Chlorine is a chemical element with the symbol Cl and atomic number 17. The second-lightest of the halogens, it appears between fluorine and bromine in the periodic table and its properties are mostly intermediate between them.\
         Chlorine has a pungent, irritating odor similar to bleach that is detectable at low concentrations. The density of chlorine gas is approximately 2.5 times greater than air, which will cause it to initially remain near the ground in areas with little air movement.")
+    time.sleep(10)
     fig = px.histogram(df,x='Cl',y=df['Cl'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -385,6 +394,7 @@ if uploaded_file is not None:
     st.markdown("The sulfate or sulphate ion is a polyatomic anion with the empirical formula SO2−4. Salts, acid derivatives, and peroxides of sulfate are widely used in industry. Sulfates occur widely in everyday life. Sulfates are salts of sulfuric acid and many are prepared from that acid. ")
     runvoice("The sulfate or sulphate ion is a polyatomic anion with the empirical formula SO2−4. Salts, acid derivatives, and peroxides of sulfate are widely used in industry. Sulfates occur widely in everyday life. Sulfates are salts of sulfuric acid and many are prepared from that acid. ")
 
+    time.sleep(10)
     fig = px.histogram(df,x='SO4',y=df['SO4'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -411,7 +421,7 @@ if uploaded_file is not None:
     st.image(Image.open("co3.png"))
     st.markdown("A carbonate is a salt of carbonic acid, characterized by the presence of the carbonate ion, a polyatomic ion with the formula CO2−3. The word carbonate may also refer to a carbonate ester, an organic compound containing the carbonate group C(O–)")
     runvoice("A carbonate is a salt of carbonic acid, characterized by the presence of the carbonate ion, a polyatomic ion with the formula CO2−3. The word carbonate may also refer to a carbonate ester, an organic compound containing the carbonate group C(O–)")
-
+    time.sleep(10)
     fig = px.histogram(df,x='CO3',y=df['CO3'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -435,6 +445,7 @@ if uploaded_file is not None:
         It's a byproduct of your body's metabolism. Our blood brings bicarbonate to our lungs, and then it is exhaled as carbon dioxide. Our kidneys also help regulate bicarbonate. Bicarbonate is excreted and reabsorbed by our kidneys.")
     runvoice("In inorganic chemistry, bicarbonate is an intermediate form in the deprotonation of carbonic acid. It is a polyatomic anion with the chemical formula HCO⁻ ₃. Bicarbonate serves a crucial biochemical role in the physiological pH buffering system.\
         It's a byproduct of your body's metabolism. Our blood brings bicarbonate to our lungs, and then it is exhaled as carbon dioxide. Our kidneys also help regulate bicarbonate. Bicarbonate is excreted and reabsorbed by our kidneys.")
+    time.sleep(10)
     fig = px.histogram(df,x='HCO3',y=df['HCO3'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -463,6 +474,7 @@ if uploaded_file is not None:
         Fluorine is a nonmetallic, pale yellow-green gaseous element with a pungent odor. It is the most electronegative and reactive of all the elements. Fluorine is an element that is widely distributed in the environment, but because of its high reactivity it is not found naturally in its elemental state.")
     runvoice("Fluorine is a chemical element with the symbol F and atomic number 9. It is the lightest halogen and exists at standard conditions as a highly toxic, pale yellow diatomic gas. As the most electronegative reactive element, it is extremely reactive, as it reacts with all other elements except for the light inert gases.\
         Fluorine is a nonmetallic, pale yellow-green gaseous element with a pungent odor. It is the most electronegative and reactive of all the elements. Fluorine is an element that is widely distributed in the environment, but because of its high reactivity it is not found naturally in its elemental state.")
+    time.sleep(10)
     fig = px.histogram(df,x='F',y=df['F'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -489,6 +501,7 @@ if uploaded_file is not None:
     st.image(Image.open("PH_scale_3.jpg"))
     st.markdown("In chemistry, pH, also referred to as acidity, historically denoting 'potential of hydrogen', is a scale used to specify the acidity or basicity of an aqueous solution. Acidic solutions are measured to have lower pH values than basic or alkaline solutions.")
     runvoice("In chemistry, pH, also referred to as acidity, historically denoting 'potential of hydrogen', is a scale used to specify the acidity or basicity of an aqueous solution. Acidic solutions are measured to have lower pH values than basic or alkaline solutions.")
+    time.sleep(10)
     fig = px.histogram(df,x='pH_GEN',y=df['pH_GEN'],color='potability',template='plotly_white',
                   marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                   barmode='group',histfunc='count')
@@ -517,6 +530,7 @@ if uploaded_file is not None:
     st.image(Image.open("ec.jpg"))
     st.markdown("The conductivity of water is a measure of the capability of water to pass electrical flow. This ability directly depends on the concentration of conductive ions in the water.")
     runvoice("The conductivity of water is a measure of the capability of water to pass electrical flow. This ability directly depends on the concentration of conductive ions in the water.")
+    time.sleep(10)
     fig = px.histogram(df,x='EC_GEN',y=df['EC_GEN'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -543,6 +557,7 @@ if uploaded_file is not None:
     st.image("hardness-of-water-1.png")
     st.markdown("Hard water is water that has high mineral content. Hard water is formed when water percolates through deposits of limestone, chalk or gypsum, which are largely made up of calcium and magnesium carbonates, bicarbonates and sulfates. Hard drinking water may have moderate health benefits. ")
     runvoice("Hard water is water that has high mineral content. Hard water is formed when water percolates through deposits of limestone, chalk or gypsum, which are largely made up of calcium and magnesium carbonates, bicarbonates and sulfates. Hard drinking water may have moderate health benefits. ")
+    time.sleep(10)
     fig = px.histogram(df,x='HAR_Total',y=df['HAR_Total'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -574,6 +589,7 @@ if uploaded_file is not None:
     st.image(Image.open("sar.png"))
     st.markdown("Sodium adsorption ratio (SAR) means a value representing the relative amount of sodium ions to the combined amount of calcium and magnesium ions in water using the following formula: SAR = [Na]/(([Ca]+[Mg])/2)1/2, where all concentrations are expressed as milliequivalents of charge per liter.")
     runvoice("Sodium adsorption ratio (SAR) means a value representing the relative amount of sodium ions to the combined amount of calcium and magnesium ions in water using the following formula: SAR = [Na]/(([Ca]+[Mg])/2)1/2, where all concentrations are expressed as milliequivalents of charge per liter.")
+    time.sleep(10)
     fig = px.histogram(df,x='SAR',y=df['SAR'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -600,6 +616,7 @@ if uploaded_file is not None:
     st.image(Image.open("eqn_rsc.png"))
     st.markdown("Residual sodium carbonate (RSC) is a common means of assessing the sodium permeability hazard, and takes into account the bicarbonate/carbonate and calcium/magnesium concentrations in irrigation water")
     runvoice("Residual sodium carbonate (RSC) is a common means of assessing the sodium permeability hazard, and takes into account the bicarbonate/carbonate and calcium/magnesium concentrations in irrigation water")
+    time.sleep(10)
     fig = px.histogram(df,x='RSC',y=df['RSC'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -626,6 +643,7 @@ if uploaded_file is not None:
     st.image(Image.open("nap.png"))
     st.markdown("It says the amount of Sodium Dissolved in liquid in terms of Percentage")
     runvoice("It says the amount of Sodium Dissolved in liquid in terms of Percentage")
+    time.sleep(10)
     fig = px.histogram(df,x='Na%',y=df['Na%'],color='potability',template='plotly_white',
                       marginal='box',opacity=0.7,nbins=100,color_discrete_sequence=[colors_green[3],colors_blue[3]],
                       barmode='group',histfunc='count')
@@ -664,6 +682,7 @@ if uploaded_file is not None:
     st.markdown("### Correlation between Variables used here")
     st.markdown("In statistics, correlation or dependence is any statistical relationship, whether causal or not, between two random variables or bivariate data.")
     runvoice("In statistics, correlation or dependence is any statistical relationship, whether causal or not, between two random variables or bivariate data.")
+    time.sleep(10)
     fig = px.imshow(cor,height=800,width=800,color_continuous_scale=colors_blue,template='plotly_white')
 
     fig.update_layout(font_family='monospace',
@@ -690,7 +709,7 @@ if uploaded_file is not None:
 
     st.markdown("#### Details of Parameters which leads potability to O")
     runvoice("Details of Parameters which leads potability to O")
-
+    time.sleep(10)
     st.write(df[df['potability']==0].describe())
 
 
@@ -698,7 +717,7 @@ if uploaded_file is not None:
 
     st.markdown("#### Details of Parameters which leads potability to 1")    
     runvoice("Details of Parameters which leads potability to 1")
-
+    time.sleep(10)
     st.write(df[df['potability']==1].describe())
 
 
@@ -779,7 +798,7 @@ if uploaded_file is not None:
 
     st.markdown("#### Best Algorithm for Supervised Machine Learning for our Data ")
     runvoice("Best Algorithm for Supervised Machine Learning for our Data")
-
+    time.sleep(10)
     finalResults
 
     df=list(df)

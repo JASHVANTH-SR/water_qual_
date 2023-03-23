4d1def1900a5f316e5b2267cbb26e615c2535005
JASHVANTH-SR <128201154+JASHVANTH-SR@users.noreply.github.com>
2023-03-23 22:56:23 +0530

    Update vlp.py

--- vlp.py
+++ vlp.py
@@ -143,7 +143,7 @@ colors_blue = ["#132C33", "#264D58", '#17869E', '#51C4D3', '#B4DBE9']
 colors_dark = ["#1F1F1F", "#313131", '#636363', '#AEAEAE', '#DADADA']
 colors_green = ['#01411C','#4B6F44','#4F7942','#74C365','#D0F0C0']
 new_file = st.file_uploader("Choose an Excel file", type="xlsx")
-st.write('''[Example CSV input file](https://docs.google.com/spreadsheets/d/1_u6TGnnj0Xs-Lkwde2H5MJ4i1o7Trixi/edit?usp=sharing&ouid=114232663325308153395&rtpof=true&sd=true)''')
+st.write('''[Example Excel(.xlsx) input file](https://docs.google.com/spreadsheets/d/1_u6TGnnj0Xs-Lkwde2H5MJ4i1o7Trixi/edit?usp=sharing&ouid=114232663325308153395&rtpof=true&sd=true)''')
 if new_file is not None:
     st.session_state.data = pd.read_excel(new_file, engine="openpyxl")    
     @st.cache(allow_output_mutation=True)    

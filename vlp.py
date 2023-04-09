db87760cabe6b77826683e86f2b9cdfcaeeed408
JASHVANTH-SR <128201154+JASHVANTH-SR@users.noreply.github.com>
2023-04-09 22:06:40 +0530

    Update vlp.py

--- vlp.py
+++ vlp.py
@@ -1,6 +1,5 @@
 # In[1]:
 import streamlit as st
-from streamlit_run_once import run_once
 
 import os
 import time
@@ -145,7 +144,7 @@ time.sleep(2)
 colors_blue = ["#132C33", "#264D58", '#17869E', '#51C4D3', '#B4DBE9']
 colors_dark = ["#1F1F1F", "#313131", '#636363', '#AEAEAE', '#DADADA']
 colors_green = ['#01411C','#4B6F44','#4F7942','#74C365','#D0F0C0']
-@run_once
+@st.experimental_singleton
 def app():
     new_file = st.file_uploader("Choose an Excel file", type="xlsx")
     st.write('''[Example Excel(.xlsx) input file](https://docs.google.com/spreadsheets/d/1_u6TGnnj0Xs-Lkwde2H5MJ4i1o7Trixi/edit?usp=sharing&ouid=114232663325308153395&rtpof=true&sd=true)''')

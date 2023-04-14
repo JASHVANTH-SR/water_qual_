c0fd57291457b7bce01331ac6aebe03d76a2e9a6
JASHVANTH-SR <128201154+JASHVANTH-SR@users.noreply.github.com>
2023-04-14 22:20:10 +0530

    Update chatbot.py

--- Water_qual/chatbot.py
+++ Water_qual/chatbot.py
@@ -4,14 +4,12 @@ import nltk
 
 # Download NLTK data
 nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
 
 # Function to check for keyword match in input text
+def get_session_state():
+    return st.session_state
 
-def app():
-    def check_keywords(input_text, data):
+def check_keywords(input_text, data):
     input_text = input_text.lower()
     for index, row in data.iterrows():
         keywords = row['keywords'].split('|')
@@ -20,23 +18,22 @@ def app():
                 return row['responses']
     return None
 
-    # Function to perform NLP on input text using NLTK
-    def perform_nlp(input_text):
-        tokens = nltk.word_tokenize(input_text)
-        pos_tags = nltk.pos_tag(tokens)
-        entities = nltk.ne_chunk(pos_tags)
-        return tokens, pos_tags, entities
+# Function to perform NLP on input text using NLTK
+def perform_nlp(input_text):
+    tokens = nltk.word_tokenize(input_text)
+    pos_tags = nltk.pos_tag(tokens)
+    entities = nltk.ne_chunk(pos_tags)
+    return tokens, pos_tags, entities
 
-    # Streamlit app
-    def get_session_state():
-        return st.session_state
-
+# Streamlit app
+def app():
     st.title("Chatbot")
 
     # Create or get the SessionStateMixin object
     session_state = get_session_state()
 
     # Create or retrieve the conversation list from the SessionStateMixin
+    data = pd.read_csv('chatbot_data.csv')
     if 'conversations' not in session_state:
         session_state.conversations = []
     conversations = session_state.conversations
@@ -57,8 +54,6 @@ def app():
 
     st.text_area("Conversation:", value="\n".join(conversations), height=300)
 
-    # Function to create or retrieve the SessionStateMixin object
+# Function to create or retrieve the SessionStateMixin object
 
-    # Load the CSV data
-    data = pd.read_csv('chatbot_data.csv')
-
+# Load the CSV data
